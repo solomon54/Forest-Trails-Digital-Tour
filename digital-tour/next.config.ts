f87054681acabdf2620ee0597b453f0e1aedf01e
solomon54 <solomontsehay54@gmail.com>
2026-01-02 18:19:30 +0300

    working on deployment setup

--- digital-tour/next.config.ts
+++ digital-tour/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
-  swcMinify: true,
 
   images: {
     // Add your domains here if needed
